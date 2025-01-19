9ff2cc6b883d8d5db1552057b66ffe2c33e8b9d0
Nivriti Raisingh <122734463+n1vr1t1@users.noreply.github.com>
2025-01-19 12:55:44 +0100

    Delete sequential.h

--- sequential.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEQUENTIAL_H
-#define SEQUENTIAL_H
-
-bool checkSym(float* M, int n);
-void matTranspose(float* M, float* T, int n);
-void matTransposeBlock(float* M, float* T, int n);
-void check_transpose(float* M, float* T, int n);
-#endif
